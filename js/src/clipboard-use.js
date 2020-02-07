e21e7cdad1d2000972d64005f716132e69f88e4e
wujiahong1998 <wujiahong2013@hotmail.com>
2020-02-07 19:36:01 +0800

    Site updated: 2020-02-07 19:35:51

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 07 2020 19:25:16 GMT+0800 (GMT+08:00)
+// build time:Fri Feb 07 2020 19:35:43 GMT+0800 (GMT+08:00)
 !function(n,t,o){var e=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">',n+="<span>复制</span>",n+="</button>",$(".highlight .code pre").before(n),new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};e()}(window,document);
 //rebuild by neat 
