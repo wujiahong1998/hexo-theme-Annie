4604e9c182d87ac26c254b6968b8dca865e0bf03
wujiahong1998 <wujiahong2013@hotmail.com>
2020-02-08 01:44:46 +0800

    Site updated: 2020-02-08 01:44:30

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 07 2020 19:35:43 GMT+0800 (GMT+08:00)
+// build time:Sat Feb 08 2020 01:44:23 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
 //rebuild by neat 
