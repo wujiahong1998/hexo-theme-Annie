c1e844e182649dbbfb2a6f82e365c53518d500f2
wujiahong1998 <wujiahong2013@hotmail.com>
2020-02-09 02:09:43 +0800

    Site updated: 2020-02-09 02:09:21

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sat Feb 08 2020 01:52:56 GMT+0800 (GMT+08:00)
+// build time:Sun Feb 09 2020 02:09:09 GMT+0800 (GMT+08:00)
 window.onload=function(){var t,e=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","../../images/failure.png"),$('[rel="shortcut icon"]').attr("href","../../images/failure.png"),document.title="(●—●)喔哟，崩溃啦！",clearTimeout(t)):($('[rel="icon"]').attr("href","../../images/favicon32.png"),$('[rel="shortcut icon"]').attr("href","../../images/favicon32.png"),document.title="(/≧▽≦/)咦！页面又好了！",t=setTimeout(function(){document.title=e},2e3))})};
 //rebuild by neat 
